d6a1b332138908261ffd081967448844be216a37
Kleber Noel <42589399+klebster2@users.noreply.github.com>
2025-11-25 20:18:27 +0000

    Fix formatting in install_tmux_completion function

--- setup.sh
+++ setup.sh
@@ -32,8 +32,8 @@ if_exists_bak() {
 
 install_tmux_completion() {
     tmux -V > /dev/null 2>&1 || exit 1
-    curl "https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash" \|
-	    sed '1,10 s/^/#/' \|
+    curl "https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash" |\
+	    sed '1,10 s/^/#/' |\
 		> "${DOTFILES}/tmux.completion.bash"
 }
 
